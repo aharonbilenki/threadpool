75c67995e359c43eb555cef42c7fa6be2cb2b635
aharon100 <aharonbilenki@gmail.com>
2022-09-21 22:07:13 +0300

    update test code

--- Task/src/source/ThreadPool.java
+++ Task/src/source/ThreadPool.java
@@ -1,5 +1,16 @@
 package source;
 
+import test.TestTask;
+
 public class ThreadPool {
+
+	public ThreadPool(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void submitTask(TestTask task) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
